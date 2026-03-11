a0a50701355346991e1639112223a6e4eaa676d4
Bb-02 <1598270553@qq.com>
2026-03-11 13:43:18 +0800

    初始化 EasyLedger 项目：完成数据库连接

---
+++ src/main/java/com/bbww/easyledger/EasyLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.bbww.easyledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbww/easyledger/EasyLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbww.easyledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
